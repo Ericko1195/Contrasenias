74fdce730124ef0dbab49437d5b9997e682d243f
Ericko1195 <ericlopez@LAPTOP-QHA13BCD.localdomain>
2023-06-16 12:52:17 -0600

    Modificaciones correspondientes a la semana 3 y hasta CI.yaml de semana 4, con canal de uibcdf

---
+++ contrasenias/tests/test_import.py
@@ -0,0 +1,12 @@
+"""
+Unit and regression test for the molsysmt package.
+"""
+
+# Import package, test suite, and other packages as needed
+import contrasenias
+import pytest
+import sys
+
+def test_contrasenias_imported():
+    """Sample test, will always pass so long as import statement worked"""
+    assert "contrasenias" in sys.modules
